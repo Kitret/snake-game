014a2e2412d4e6ef20b198f6f78f008163aed0e8
Kitret <rahuljha29052002@gmail.com>
2023-12-18 14:50:30 +0530

    Added functionality for collision detection and function to start and reset the game, fixed the speed increase issue where speed increased very fast, fuctionality to update the current score and keeping track of the high score.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const board = document.getElementById("game-board");
 const instructionText = document.getElementById("instruction-text");
 const logo = document.getElementById("logo");
+const scoreText = document.getElementById("score");
+const highScoreText = document.getElementById("highScore");
 
 // define game variables
 const gridSize = 20;
@@ -11,6 +13,7 @@ let food = generateFood();
 let gameInterval;
 let gameSpeedDelay = 200;
 let gameStarted=false;
+let highScore=0;
 
 // Draw game map, snake, food
 function draw() {
@@ -46,9 +49,11 @@ function setPosition(element, position) {
 
 // draw food on the board
 function drawFood() {
-    const foodElement = createGameElement("div","food");
-    setPosition(foodElement,food);
-    board.appendChild(foodElement);
+    if(gameStarted){
+        const foodElement = createGameElement("div","food");
+        setPosition(foodElement,food);
+        board.appendChild(foodElement);
+    }
 }
 
 // generate food position at random
@@ -81,10 +86,13 @@ function move() {
     
     if(head.x==food.x && head.y==food.y) {
         food = generateFood();
-        clearInterval(); // clear past interval
+        increaseSpeed();
+        clearInterval(gameInterval); // clear past interval
         gameInterval = setInterval(() => {
             move();
+            checkCollision();
             draw();
+            updateScore();
         },gameSpeedDelay);
 
     } else {
@@ -105,7 +113,7 @@ function startGame() {
     logo.style.display = "none";
     gameInterval = setInterval(() => {
         move();
-        // checkCollision();
+        checkCollision();
         draw();
     },gameSpeedDelay);
 }
@@ -119,7 +127,7 @@ function handleKeyPress(event) {
         if(event.key==="ArrowUp") {
             direction="up";
         }
-        else if(event.key==="ArrowDowm") {
+        else if(event.key==="ArrowDown") {
             direction="down";
         }
         else if(event.key==="ArrowLeft") {
@@ -132,4 +140,63 @@ function handleKeyPress(event) {
             // do nothing for other keys
         }
     }
+}
+
+document.addEventListener("keydown",handleKeyPress);
+
+function increaseSpeed() {
+    if(gameSpeedDelay>150) {
+        gameSpeedDelay+=5;
+    }
+    else if(gameSpeedDelay<100) {
+        gameSpeedDelay-=3;
+    }
+    else if(gameSpeedDelay<50) {
+        gameSpeedDelay-=2;
+    }
+    else if(gameSpeedDelay<35) {
+        gameSpeedDelay-=1;
+    }
+}
+
+function checkCollision() {
+    const head=snake[0];
+    if(head.x<=0 || head.y<=0 || head.x>gridSize || head.y>gridSize) {
+        resetGame();
+    }
+    for(let i=1;i<snake.length;i++) {
+        if(head.x===snake[i].x && head.y===snake[i].y) {
+            resetGame();
+        }
+    }
+    return ;
+}
+
+function resetGame() {
+    updateHighScore();
+    stopGame();
+    snake=[{x:10,y:10}];
+    food = generateFood();
+    direction="right";
+    gameSpeedDelay=200;
+    updateScore();
+}
+
+function updateScore() {
+    const currentScore=snake.length-1;
+    scoreText.textContent=currentScore.toString().padStart(3,"0");
+}
+
+function stopGame() {
+    clearInterval(gameInterval);
+    gameStarted = false;
+    instructionText.style.display = "block";
+    logo.style.display = "block";
+}
+
+function updateHighScore() {
+    const currentScore = snake.length - 1;
+    highScore=Math.max(currentScore,highScore);
+    highScoreText.textContent = highScore.toString().padStart(3,"0");
+    highScoreText.style.display="block";
 }
