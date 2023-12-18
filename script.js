18134372aba2cc10c59b920e555f5db66f5b8db2
Kitret <rahuljha29052002@gmail.com>
2023-12-18 12:24:59 +0530

    Adding the functionality for drawing the food, moving the  snake and key listners for the arrow keys

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // define html elements to manipulate from index.html file
 const board = document.getElementById("game-board");
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
 
 // define game variables
+const gridSize = 20;
 let snake = [{x:10, y:10}];
+let direction = "right";
+let food = generateFood();
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted=false;
 
 // Draw game map, snake, food
 function draw() {
     board.innerHTML="";
     drawSnake();
+    drawFood();
 }
 
 // draw the snake
@@ -32,4 +41,95 @@ function setPosition(element, position) {
     element.style.gridRow = position.y;
 }
 
-draw();
+// testing draw function
+// draw();
+
+// draw food on the board
+function drawFood() {
+    const foodElement = createGameElement("div","food");
+    setPosition(foodElement,food);
+    board.appendChild(foodElement);
+}
+
+// generate food position at random
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize)+1;
+    const y = Math.floor(Math.random() * gridSize)+1;
+    return {x,y};
+}
+
+// testing drawFood 
+// drawFood();
+
+// moving the snake
+function move() {
+    const head = {...snake[0]};
+    if(direction=="right") {
+        head.x++;
+    }
+    else if(direction=="left") {
+        head.x--;
+    }
+    else if(direction=="up") {
+        head.y--;
+    }
+    else if(direction=="down") {
+        head.y++;
+    }
+
+    snake.unshift(head);
+    
+    if(head.x==food.x && head.y==food.y) {
+        food = generateFood();
+        clearInterval(); // clear past interval
+        gameInterval = setInterval(() => {
+            move();
+            draw();
+        },gameSpeedDelay);
+
+    } else {
+        snake.pop();
+    }
+}
+
+// testing the move function
+// setInterval(() => {
+//     move(); // move the snake
+//     draw(); // draw the snake again
+// },200);
+
+// start game function
+function startGame() {
+    gameStarted=true; // keep track of running game
+    instructionText.style.display = "none";
+    logo.style.display = "none";
+    gameInterval = setInterval(() => {
+        move();
+        // checkCollision();
+        draw();
+    },gameSpeedDelay);
+}
+
+// key Press event listener
+function handleKeyPress(event) {
+    if(!gameStarted && (event.code==="Space" || event.key===" ")) {
+        startGame();
+    }
+    else {
+        if(event.key==="ArrowUp") {
+            direction="up";
+        }
+        else if(event.key==="ArrowDowm") {
+            direction="down";
+        }
+        else if(event.key==="ArrowLeft") {
+            direction="left";
+        }
+        else if(event.key==="ArrowRight") {
+            direction="right";
+        }
+        else {
+            // do nothing for other keys
+        }
+    }
+}
